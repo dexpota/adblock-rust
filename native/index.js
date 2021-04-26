a5e8d238c9610999203fa0e9248cf77f8d6bee7a
Anton Lazarev <antonok35@gmail.com>
2021-04-26 13:55:06 -0700

    update filterset bindings

--- native/index.js
+++ native/index.js
@@ -1,7 +1,25 @@
 const blocker = require('./index.node');
 
+class FilterSet {
+    constructor(debug) {
+        this.boxed = blocker.FilterSet_constructor(debug);
+    }
+
+    addFilters(filters, format) {
+        return blocker.FilterSet_addFilters(this.boxed, filters, format);
+    }
+
+    addFilter(filter, format) {
+        return blocker.FilterSet_addFilter(this.boxed, filter, format);
+    }
+
+    intoContentBlocking(rule_types) {
+        return blocker.FilterSet_intoContentBlocking(this.boxed, rule_types);
+    }
+}
+
 exports.FilterFormat = blocker.FilterFormat;
-exports.FilterSet = blocker.FilterSet;
+exports.FilterSet = FilterSet;
 exports.RuleTypes = blocker.RuleTypes;
 exports.Engine = blocker.Engine;
 exports.uBlockResources = blocker.uBlockResources;
