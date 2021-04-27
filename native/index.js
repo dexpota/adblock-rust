f97e2c5b6f663f192473b518cc77adc155628e4f
Anton Lazarev <antonok35@gmail.com>
2021-04-26 22:12:27 -0700

    update engine bindings

--- native/index.js
+++ native/index.js
@@ -18,8 +18,50 @@ class FilterSet {
     }
 }
 
+class Engine {
+    constructor() {
+        this.boxed = blocker.Engine_constructor(filter_set, config);
+    }
+
+    check(url, source_url, request_type, debug) {
+        return blocker.Engine_check(this.boxed, url, source_url, request_type, debug);
+    }
+
+    serialize() {
+        return blocker.Engine_serialize(this.boxed);
+    }
+
+    deserialize(buffer) {
+        return blocker.Engine_deserialize(this.boxed, buffer);
+    }
+
+    enableTag(tag) {
+        return blocker.Engine_enableTag(this.boxed, tag);
+    }
+
+    useResources(resources) {
+        return blocker.Engine_useResources(this.boxed, resources);
+    }
+
+    tagExists(tag) {
+        return blocker.Engine_tagExists(this.boxed, tag);
+    }
+
+    clearTags() {
+        return blocker.Engine_clearTags(this.boxed);
+    }
+
+    addResource(resource) {
+        return blocker.Engine_addResource(this.boxed, resource);
+    }
+
+    getResource(name) {
+        return blocker.Engine_getResource(this.boxed, name);
+    }
+}
+
 exports.FilterFormat = blocker.FilterFormat;
 exports.FilterSet = FilterSet;
 exports.RuleTypes = blocker.RuleTypes;
-exports.Engine = blocker.Engine;
+exports.Engine = Engine;
 exports.uBlockResources = blocker.uBlockResources;
